2330477c69002795bdf76843f056b4c54f8dff61
Neetesh Dadwariya <neeteshdadwariya@gmail.com>
2022-01-15 00:05:57 -0600

    Removed DEV mapping for heroku app

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,8 +1,10 @@
-const BE_SERVER = 'localhost:3000';
+const BE_SERVER = 'https://neetesh-todo.herokuapp.com';
+// DEV mode
+// const BE_SERVER = 'http://localhost:3000';
 
 export const API_PATHS = {
-    GET_ALL_TODOS: `http://${BE_SERVER}/api/get-all-todos`,
-    COMPLETE_TODO: `http://${BE_SERVER}/api/complete-todo/{todo_id}`,
-    DELETE_TODO: `http://${BE_SERVER}/api/delete-todo/{todo_id}`,
-    ADD_TODO: `http://${BE_SERVER}/api/add-todo`
+    GET_ALL_TODOS: `${BE_SERVER}/api/get-all-todos`,
+    COMPLETE_TODO: `${BE_SERVER}/api/complete-todo/{todo_id}`,
+    DELETE_TODO: `${BE_SERVER}/api/delete-todo/{todo_id}`,
+    ADD_TODO: `${BE_SERVER}/api/add-todo`
 }
